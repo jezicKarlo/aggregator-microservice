ffc1e31d7af8607eb3d02e4ed6419f067b063fc3
jezicKarlo <karlo.jezic@gmail.com>
2021-01-06 13:07:26 +0100

    register yourself to eureka and get config from eureka

--- src/main/java/hr/fer/rassus/lti/aggregatormicroservice/controller/EntryPoint.java
+++ src/main/java/hr/fer/rassus/lti/aggregatormicroservice/controller/EntryPoint.java
@@ -3,10 +3,12 @@ package hr.fer.rassus.lti.aggregatormicroservice.controller;
 import hr.fer.rassus.lti.aggregatormicroservice.config.ConfigurationData;
 import hr.fer.rassus.lti.aggregatormicroservice.response.Reading;
 import hr.fer.rassus.lti.aggregatormicroservice.service.ReadingsService;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@EnableDiscoveryClient
 public class EntryPoint {
 
     private ReadingsService service;
